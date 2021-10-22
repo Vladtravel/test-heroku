26da61729c163f91b8d200f9c8417b0df75f84d1
Vladyslav <vladtravel2015@gmail.com>
2021-10-22 14:12:49 +0300

    make it better

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "11" });
+  res.json({ message: "22" });
 });
 
 router.get("/:contactId", async (req, res, next) => {
