69e2c8ca3481b861fc14b9bbe700df2aac1d4148
Vladyslav <vladtravel2015@gmail.com>
2021-10-22 15:03:18 +0300

    make it better

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const boolParser = require("express-query-boolean");
+const { HttpCode } = require("./service/constants");
 
-const contactsRouter = require('./routes/api/contacts')
+const usersRouter = require("./routes/api/users");
+const contactsRouter = require("./routes/api/contacts");
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(helmet());
+app.use(logger(formatsLogger));
+app.get("env") !== "test" && app.use(logger(formatsLogger));
+app.use(express.static("public"));
 
-app.use('/api/contacts', contactsRouter)
+app.use(cors());
+app.use(express.json({ limit: 10000 }));
+app.use(boolParser());
+
+app.use("/api/users", usersRouter);
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(HttpCode.NOT_FOUND).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  const status = err.status || HttpCode.INTERNAL_SERVER_ERROR;
+  res.status(status).json({
+    status: status === HttpCode.INTERNAL_SERVER_ERROR ? "fail" : "error",
+    code: status,
+    message: err.message,
+  });
+});
 
-module.exports = app
+module.exports = app;
