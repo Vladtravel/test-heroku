402511ee5c70c38fc258f131266e8e3d4a768b49
Vladyslav <vladtravel2015@gmail.com>
2021-10-22 15:24:34 +0300

    make it better

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use(cors());
 app.use(express.json({ limit: 10000 }));
 app.use(boolParser());
 
-app.use("/api/users", usersRouter);
-app.use("/api/contacts", contactsRouter);
+// app.use("/api/users", usersRouter);
+// app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
   res.status(HttpCode.NOT_FOUND).json({ message: "Not found" });
