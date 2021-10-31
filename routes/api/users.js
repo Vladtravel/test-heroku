f94c81cdc99ff3c581b3a8e092b193cdc66222b6
Vladyslav <vladtravel2015@gmail.com>
2021-10-31 18:39:13 +0200

    make it better

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,7 +10,7 @@ const uploadAvatar = require("../../service/upload-avatar");
 const rateLimit = require("express-rate-limit");
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 3, // limit each IP to 100 requests per windowMs
+  max: 20, // limit each IP to 100 requests per windowMs
   handler: (req, res, next) => {
     return res.status(HttpCode.TOO_MANY_REQUESTS).json({
       status: "error",
